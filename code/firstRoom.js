4582842236320d779c9957157b4648999ee693fe
David Figatner <david@yopeyopey.com>
2022-02-03 19:57:38 -0800

    dalia's adventure

--- code/firstRoom.js
+++ code/firstRoom.js
@@ -2,14 +2,22 @@ import { say } from "./say.js"
 
 class FirstRoom {
     enter() {
-        say("Welcome to the first room! What are you going to do???")
+        say("The woods are dark and dreary. You hear the howls of various wild animals around you. Do you look for food or a shelter?")
     }
 
+
     respond(text) {
-        if (text.includes("look")) {
-            say("you look around!")
+        text = text.toLowerCase()
+        if (text.includes("food")) {
+            say("You decide to look for food, but it will not be easy. Suddenly, as you walk amongst the trees, you see some delicious-looking fungi! Do you pick it, eat it, or ignore it?")
+        } else if (text.includes("pick")) {
+            say("You decide to pick the fungi. Added 3 fungi to inventory.")
+        } else if (text.includes("shelter")) {
+            say("You decide to look for a shelter. As you walk, you realize that the trees seem shorter than you had thought, and actually not that tall. Do you climb a tree to camp out for the night?")
+        } else if (text.includes("climb")) {
+            say("You decide to climb a tree. You find one, and start to climb.")
         } else {
-            say("I did not understand that!")
+            say("Huh?")
         }
     }
 }
